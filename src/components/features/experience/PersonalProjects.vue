74ead842f2ea1a7861e08993e5b5f23b3dc39b21
Luc van Deenen <44636426+LucvanDeenen@users.noreply.github.com>
2024-11-03 17:06:48 +0100

    added one description

--- src/components/features/experience/PersonalProjects.vue
+++ src/components/features/experience/PersonalProjects.vue
@@ -6,10 +6,11 @@ const projects = [
     title: "Portfolio Website",
     subtitle: "Personal Website - Dedeen.nl",
     description: [
-      "A responsive portfolio website built with Vue 3 and Tailwind CSS",
-      "Optimized for performance and accessibility"
+      "A responsive portfolio website built with Vue 3 and Tailwind CSS.",
+      "Setup automatic deployment with Github Actions and Firebase hosting.",
+      "Optimized for performance and accessibility."
     ],
-    technologies: ["Vue 3", "TypeScript", "Tailwind CSS"]
+    technologies: ["Vue 3", "TypeScript", "Tailwind CSS", "Github Actions"]
   }
 ]
 </script>
