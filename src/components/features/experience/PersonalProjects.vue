4b7484cc05db577161bcc220b49c733c8d57300d
Luc van Deenen <44636426+LucvanDeenen@users.noreply.github.com>
2025-01-02 23:04:21 +0100

    added new project

--- src/components/features/experience/PersonalProjects.vue
+++ src/components/features/experience/PersonalProjects.vue
@@ -3,6 +3,20 @@ import Timeline from '../../shared/Timeline.vue'
 
 const projects = [
   {
+    title: "Party App",
+    links: {
+      github: "https://github.com/LucvanDeenen/PartyApp",
+      demo: "https://sjoelen-39112.web.app/"
+    },
+    subtitle: "A party games app!",
+    description: [
+      "Build an application for keeping track of scores in party games.",
+      "Setup authentication and storage in firebase.",
+      "Created an android App for Smart TV's to showcase the hiscores and games on your TV!"
+    ],
+    technologies: ["Vue 3", "TypeScript", "Firebase", "Github Actions", "Kotlin", "Android Studio"]
+  },
+  {
     title: "Portfolio Website",
     subtitle: "Personal Website - Dedeen.nl",
     description: [
