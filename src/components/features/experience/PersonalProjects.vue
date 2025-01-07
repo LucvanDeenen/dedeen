1d64ac39a38363f6915f6d8e48919cb4a68efbe5
lucvandeenen <lucvd@hotmail.nl>
2025-01-07 11:21:21 +0100

    updated projects

--- src/components/features/experience/PersonalProjects.vue
+++ src/components/features/experience/PersonalProjects.vue
@@ -3,6 +3,20 @@ import Timeline from '../../shared/Timeline.vue'
 
 const projects = [
   {
+    title: "Cooking App",
+    links: {
+      github: "https://github.com/LucvanDeenen/munch",
+      demo: "https://cooking-d8911.web.app/"
+    },
+    subtitle: "An app used to aid in cooking!",
+    description: [
+      "The application keeps track of certain ingredients and then shows you which recipes you can make.",
+      "The recipes contain step-by-step instructions on how to make them.",
+      "Setup storage using and deployed with firebase."
+    ],
+    technologies: ["Vue 3", "TypeScript", "Firebase", "Github Actions", "Tailwind"]
+  },
+  {
     title: "Party App",
     links: {
       github: "https://github.com/LucvanDeenen/PartyApp",
@@ -12,9 +26,10 @@ const projects = [
     description: [
       "Build an application for keeping track of scores in party games.",
       "Setup authentication and storage in firebase.",
-      "Created an android App for Smart TV's to showcase the hiscores and games on your TV!"
+      "Created an android App for Smart TV's to showcase the hiscores and games on your TV!",
+      "Used different AI tools like, ChatGPT and Bolt to see how fast development can be when making the most of these tools."
     ],
-    technologies: ["Vue 3", "TypeScript", "Firebase", "Github Actions", "Kotlin", "Android Studio"]
+    technologies: ["Vue 3", "TypeScript", "Firebase", "Github Actions", "Kotlin", "Android Studio", "Bolt"]
   },
   {
     title: "Portfolio Website",
