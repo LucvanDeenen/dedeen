57b56c09f6930102a361ff13cfe04d2cd6e02e20
Luc van Deenen <44636426+LucvanDeenen@users.noreply.github.com>
2025-06-22 21:14:42 +0200

    fixing build

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/auth": {
-        target: "http://localhost:8787",
+        target: import.meta.env.VITE_BACKEND_URL,
         changeOrigin: true,
         secure: false,
       },
