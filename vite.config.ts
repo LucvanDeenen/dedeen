c39e817bcdf5a9313fc3c2e2f000109c13d6b048
Luc van Deenen <44636426+LucvanDeenen@users.noreply.github.com>
2025-06-22 21:17:38 +0200

    setup using loadEnv setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,27 @@
-// Plugins
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  server: {
-    port: 3000,
-    proxy: {
-      "/auth": {
-        target: import.meta.env.VITE_BACKEND_URL,
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [vue()],
+    server: {
+      port: 3000,
+      proxy: {
+        "/auth": {
+          target: env.VITE_BACKEND_URL,
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-  base: "./",
+    base: "./",
+  };
 });
