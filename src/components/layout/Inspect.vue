3e9afae419985e3343910778a6e83442865cf0ac
Luc van Deenen <lucvd@hotmail.nl>
2025-10-02 14:37:16 +0200

    setup header to popout modal

---
+++ src/components/layout/Inspect.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { Squares2X2Icon } from "@heroicons/vue/24/outline";
+const emit = defineEmits(["popout"]);
+
+function handlePopout() {
+  emit("popout");
+}
+</script>
+
+<template>
+  <div class="ml-auto">
+    <button type="button" class="btn btn-outline-primary" @click="handlePopout">
+      <Squares2X2Icon class="sm:w-10 w-7 mr-1 text-yellow-500" />
+    </button>
+  </div>
+</template>
--- src/pages/ProjectPage.vue
+++ src/pages/ProjectPage.vue
@@ -1,10 +1,20 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import Page from "@/components/layout/Page.vue";
 import PersonalProjects from "@/components/content/projects/PersonalProjects.vue";
+import PersonalDemos from "@/components/content/projects/PersonalDemos.vue";
+import Inspect from "@/components/layout/Inspect.vue";
+
+const show = ref(false);
 </script>
 
 <template>
   <Page title="Projects">
+    <template #header>
+      <Inspect @popout="show = !show" />
+    </template>
+
+    <PersonalDemos />
     <PersonalProjects />
   </Page>
 </template>
